192b551cad33d9d4d9cf858c114749279e68ca3f
sieunju <j.sieun73@gmail.com>
2023-03-31 21:32:51 +0900

    docs: til auth api docs 작성

--- server/features/til/auth/ApiRouter.js
+++ server/features/til/auth/ApiRouter.js
@@ -6,7 +6,39 @@ const router = express.Router();
 const utils = require('../../../utils/commandUtil')
 
 /**
- * EndPoint: /api/til/auth/refresh
+ * @swagger
+ *
+ * /api/til/auth/refresh:
+ *  post:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "Auth Token 갱신"
+ *    tags: [TIL]
+ *    parameters:
+ *        - in: header
+ *          name: Token
+ *          description: 토큰
+ *          schema:
+ *              type: string
+ *              example: randomtoken
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: true
+ *                  data:
+ *                      type: object
+ *                      properties:
+ *                          payload:
+ *                              type: object
+ *                              properties:
+ *                                  token:
+ *                                      type: string
+ *                                      example: Token Migane Koda Dewdy
  */
 router.post('/refresh', (req, res) => {
     // Refresh Token..
@@ -24,8 +56,32 @@ router.post('/refresh', (req, res) => {
 })
 
 /**
- * 만료되는 토큰 발급 받기
- * EndPoint: /api/til/auth/expired
+ * @swagger
+ *
+ * /api/til/auth/expired:
+ *  post:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "만료되는 토큰 발급 받기 입니다."
+ *    tags: [TIL]
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: true
+ *                  data:
+ *                      type: object
+ *                      properties:
+ *                          payload:
+ *                              type: object
+ *                              properties:
+ *                                  token:
+ *                                      type: string
+ *                                      example: Token Expired 112123123123
  */
 router.post('/expired', (req, res) => {
     var expiredToken = 'Token Expired ' + new Date().getTime()
@@ -41,7 +97,35 @@ router.post('/expired', (req, res) => {
 })
 
 /**
- * EndPoint: /api/til/auth/test
+ * @swagger
+ *
+ * /api/til/auth/test:
+ *  get:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "만료되는 토큰 발급 받기 입니다."
+ *    tags: [TIL]
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: true
+ *                  data:
+ *                      type: object
+ *                      properties:
+ *                          payload:
+ *                              type: object
+ *                              properties:
+ *                                  integer:
+ *                                      type: date-time
+ *                                      example: 112123123123
+ *                                  str:
+ *                                      type: string
+ *                                      example: 그대와 처음 만난 이곳 모든날 모든 순간 좋았다.
  */
 router.get('/test', (req, res) => {
     res.status(200).send({
