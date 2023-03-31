1ab74f949c04d39f7de915fe2940081c6ddf5d45
sieunju <j.sieun73@gmail.com>
2023-03-31 21:39:41 +0900

    docs: til error api docs 작성

--- server/features/til/error_handling/ApiRouter.js
+++ server/features/til/error_handling/ApiRouter.js
@@ -6,8 +6,27 @@ const router = express.Router();
 const utils = require('../../../utils/commandUtil')
 
 /**
- * GET
- * EndPoint: /api/til/error/505
+ * @swagger
+ *
+ * /api/til/error/505:
+ *  get:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "HTTP 505 에러 입니다."
+ *    tags: [TIL_ERROR]
+ *    responses:
+ *      505:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: false
+ *                  message:
+ *                      type: string
+ *                      example: 505 Get 방식의 Error 입니다.
+ * 
  */
 router.get('/505', (req, res) => {
     res.status(505).send({
@@ -17,8 +36,27 @@ router.get('/505', (req, res) => {
 })
 
 /**
- * POST
- * EndPoint: /api/til/error/505
+ * @swagger
+ *
+ * /api/til/error/505:
+ *  post:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "HTTP 505 에러 입니다."
+ *    tags: [TIL_ERROR]
+ *    responses:
+ *      505:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: false
+ *                  message:
+ *                      type: string
+ *                      example: 505 Post 방식의 Error 입니다.
+ * 
  */
 router.post('/505', (req, res) => {
     res.status(505).send({
@@ -28,8 +66,27 @@ router.post('/505', (req, res) => {
 })
 
 /**
- * POST
- * EndPoint: /api/til/error/404
+ * @swagger
+ *
+ * /api/til/error/404:
+ *  post:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "HTTP 505 에러 입니다."
+ *    tags: [TIL_ERROR]
+ *    responses:
+ *      505:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: false
+ *                  message:
+ *                      type: string
+ *                      example: 404 Post 방식의 Error 입니다.
+ * 
  */
 router.post('/404', (req, res) => {
     res.status(404).send({
@@ -39,8 +96,33 @@ router.post('/404', (req, res) => {
 })
 
 /**
- * GET
- * EndPoint: /api/til/error/404
+ * @swagger
+ *
+ * /api/til/error/404:
+ *  get:
+ *    summary: "TIL 전용 API 테스트 입니다."
+ *    description: "HTTP 404 에러 입니다."
+ *    tags: [TIL_ERROR]
+ *    responses:
+ *      404:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  status:
+ *                      type: boolean
+ *                      example: false
+ *                  message:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: Hello
+ *                          contents:
+ *                              type: string
+ *                              example: Error Contents
+ * 
  */
 router.get('/404', (req, res) => {
     const erroMsg = {
